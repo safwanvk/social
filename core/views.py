e48ad6738aa8ba9d6fa3227da2eab38e82958b57
safwanvk <safwanvalakundil@gmail.com>
2020-08-23 16:05:11 +0530

    :fire: Delete app core

--- core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.views.generic import TemplateView
-
-
-class IndexView(TemplateView):
-    template_name = 'core/index.html'
