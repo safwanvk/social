b2f292e7848d7074d54c584c70728af6f5a45594
safwanvk <safwanvalakundil@gmail.com>
2020-10-24 18:06:14 +0530

    :zap: Set urls  for crud posts

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,10 @@ from social.posts import views
 app_name = 'posts'
 
 urlpatterns = [
-    url('', views.PostList.as_view(), name='posts')
+    url(r'^$', views.PostList.as_view(), name='posts'),
+    url(r'new/$', views.CreatePost.as_view(), name='create'),
+    url(r'by/(?P<username>[-\w]+)', views.UserPosts.as_view(), name='for_user'),
+    url(r'by/(?P<username>[-\w]+)/(?P<pk>\d+)/$', views.PostDetail.as_view(), name='single'),
+    url(r'delete/(?P<pk>\d+)/$', views.DeletePost.as_view(), name='delete')
+
 ]
