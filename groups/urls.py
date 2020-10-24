8f6c940db4408a596ad62acf4c4de4e62123f451
safwanvk <safwanvalakundil@gmail.com>
2020-10-24 21:04:03 +0530

    :zap: Set url for join and leave group

--- groups/urls.py
+++ groups/urls.py
@@ -1,11 +1,15 @@
-from django.urls import path
+from django.conf.urls import url
+
 from . import views
 
 app_name = 'groups'
 
 urlpatterns = [
-    path('new/', views.CreateGroup.as_view(), name='create'),
-    path('posts/<slug>/', views.SingleGroup.as_view(), name='single'),
-    path('', views.ListGroups.as_view(), name='groups'),
+    url(r'^new/$', views.CreateGroup.as_view(), name='create'),
+    url(r'posts/in/(?P<slug>[-\w]+)/$', views.SingleGroup.as_view(), name='single'),
+    url(r'^$', views.ListGroups.as_view(), name='groups'),
+    url(r'join/(?P<slug>[-\w]+)/$', views.JoinGroup.as_view(), name='join'),
+    url(r'leave/(?P<slug>[-\w]+)/$', views.LeaveGroup.as_view(), name='leave')
+
 
 ]
