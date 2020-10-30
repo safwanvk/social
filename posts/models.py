2f8e2ab29418d9f485a416090569a6da67137a05
safwanvk <safwanvalakundil@gmail.com>
2020-10-30 15:03:55 +0530

    :bug: Fix bug

--- posts/models.py
+++ posts/models.py
@@ -27,7 +27,7 @@ class Post(models.Model):
         super().save(*args, **kwargs)
 
     def get_absolute_url(self):
-        return reverse('posts:single', kwargs={'username': self.user.userneme, 'pk': self.pk})
+        return reverse('posts:single', kwargs={'username': self.user.username, 'pk': self.pk})
 
     class Meta:
         ordering = ['-created_at']
